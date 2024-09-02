2e3f45b379ae040fc01607ffae684df308cacdff
Artax01 <158834512+Artax01@users.noreply.github.com>
2024-09-02 11:53:17 +0200

    update sideMenu.js

--- sources/js/sideMenu.js
+++ sources/js/sideMenu.js
@@ -2,10 +2,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
   const burger_menu = document.getElementById('burger-menu'); // burger menu
   const darkMode = document.getElementById('darkMode'); // dark-theme
 
-  const zoomQRLink = document.getElementById('zoomQRLink');
-  const zoomQRLink_2 = document.getElementById('zoomQRLink_2');
-  const zoom_menu = document.getElementById('zoom_menu');
-
   // element
   const side_menu = document.getElementById('side_menu');
   const blur = document.getElementById('blur');
@@ -15,18 +11,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
     side_menu.classList.toggle('active');
     blur.classList.toggle('active');
   });
-
-  zoomQRLink.addEventListener('click', () => {
-    zoom_menu.classList.toggle('active');
-  });
-
-  zoomQRLink_2.addEventListener('click', () => {
-    zoom_menu.classList.toggle('active');
-  });
-
-  // zoom_menu.addEventListener('click', () => {
-  //   zoom_menu.classList.toggle('active');
-  // });
   
   blur.addEventListener('click', () => {
     burger_menu.classList.toggle('active');
