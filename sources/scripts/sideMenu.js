3432f8a50e08c6fbefbadddd73ea92af5df6493c
Artax <158834512+Artax01@users.noreply.github.com>
2024-09-11 23:36:00 +0200

    update sideMenu.js

--- sources/scripts/sideMenu.js
+++ sources/scripts/sideMenu.js
@@ -1,26 +1,25 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-  const burger_menu = document.getElementById('burger-menu'); // burger menu
-  const darkMode = document.getElementById('darkMode'); // dark-theme
-
-  // element
-  const side_menu = document.getElementById('side_menu');
-  const blur = document.getElementById('blur');
-
-  burger_menu.addEventListener('click', () => {
-    burger_menu.classList.toggle('active');
-    side_menu.classList.toggle('active');
-    blur.classList.toggle('active');
-  });
-  
-  blur.addEventListener('click', () => {
-    burger_menu.classList.toggle('active');
-    side_menu.classList.toggle('active');
-    blur.classList.toggle('active');
-  });
-
-  darkMode.addEventListener('click', () => {
-    document.body.classList.toggle('dark_theme');
-    darkMode.classList.toggle('active');
-  });
-
+document.addEventListener('DOMContentLoaded', function () {
+    var burger_menu = document.getElementById('burger-menu'); // burger menu
+    var darkMode = document.getElementById('darkMode'); // dark-theme
+    // element
+    var side_menu = document.getElementById('side_menu');
+    var blur = document.getElementById('blur');
+    if (burger_menu && side_menu && blur) {
+        burger_menu.addEventListener('click', function () {
+            burger_menu.classList.toggle('active');
+            side_menu.classList.toggle('active');
+            blur.classList.toggle('active');
+        });
+        blur.addEventListener('click', function () {
+            burger_menu.classList.toggle('active');
+            side_menu.classList.toggle('active');
+            blur.classList.toggle('active');
+        });
+    }
+    if (darkMode) {
+        darkMode.addEventListener('click', function () {
+            document.body.classList.toggle('dark_theme');
+            darkMode.classList.toggle('active');
+        });
+    }
 });
