ce914761a7f0fd38b219b9dc6b2c6e05394999b6
Artax <158834512+Artax01@users.noreply.github.com>
2024-09-11 23:37:33 +0200

    Add files via upload

---
+++ sources/scripts/sideMenu.ts
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const burger_menu : HTMLElement | null = document.getElementById('burger-menu'); // burger menu
+  const darkMode : HTMLElement | null = document.getElementById('darkMode'); // dark-theme
+
+  // element
+  const side_menu : HTMLElement | null = document.getElementById('side_menu');
+  const blur : HTMLElement | null = document.getElementById('blur');
+
+  if (burger_menu && side_menu && blur) {
+    burger_menu.addEventListener('click', function () {
+      burger_menu.classList.toggle('active');
+      side_menu.classList.toggle('active');
+      blur.classList.toggle('active');
+    });
+  
+    blur.addEventListener('click', function () {
+      burger_menu.classList.toggle('active');
+      side_menu.classList.toggle('active');
+      blur.classList.toggle('active');
+    });
+  }
+
+  if (darkMode) {
+    darkMode.addEventListener('click', function () {
+      document.body.classList.toggle('dark_theme');
+      darkMode.classList.toggle('active');
+    });
+  }
+
+});
