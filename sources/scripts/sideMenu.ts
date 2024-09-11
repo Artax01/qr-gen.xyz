fc44a3c57e62a73ea77249abcc46a9502a67bbe7
Artax <158834512+Artax01@users.noreply.github.com>
2024-09-11 23:48:18 +0200

    update sideMenu.ts

--- sources/scripts/sideMenu.ts
+++ sources/scripts/sideMenu.ts
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() : void {
   const burger_menu : HTMLElement | null = document.getElementById('burger-menu'); // burger menu
   const darkMode : HTMLElement | null = document.getElementById('darkMode'); // dark-theme
 
@@ -7,13 +7,13 @@ document.addEventListener('DOMContentLoaded', function() {
   const blur : HTMLElement | null = document.getElementById('blur');
 
   if (burger_menu && side_menu && blur) {
-    burger_menu.addEventListener('click', function () {
+    burger_menu.addEventListener('click', function () : void {
       burger_menu.classList.toggle('active');
       side_menu.classList.toggle('active');
       blur.classList.toggle('active');
     });
   
-    blur.addEventListener('click', function () {
+    blur.addEventListener('click', function () : void {
       burger_menu.classList.toggle('active');
       side_menu.classList.toggle('active');
       blur.classList.toggle('active');
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   if (darkMode) {
-    darkMode.addEventListener('click', function () {
+    darkMode.addEventListener('click', function () : void {
       document.body.classList.toggle('dark_theme');
       darkMode.classList.toggle('active');
     });
