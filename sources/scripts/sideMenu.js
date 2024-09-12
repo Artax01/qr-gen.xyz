3c01b6be998d8e4514223059336f2aad019a464e
Artax <158834512+Artax01@users.noreply.github.com>
2024-09-12 20:11:45 +0200

    update sideMenu.js

--- sources/scripts/sideMenu.js
+++ sources/scripts/sideMenu.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var burger_menu = document.getElementById('burger-menu');
-    var darkMode = document.getElementById('darkMode');
+    var burger_menu = document.getElementById('burger-menu'); // burger-menu
+    var darkMode = document.getElementById('darkMode'); // theme switch button
+    // element
     var side_menu = document.getElementById('side_menu');
     var blur = document.getElementById('blur');
+    
     if (burger_menu && side_menu && blur) {
         burger_menu.addEventListener('click', function () {
             burger_menu.classList.toggle('active');
