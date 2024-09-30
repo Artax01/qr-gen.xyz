56ccd5d69ab6740a5d015f449ea47b35dcadd4fa
Artax <158834512+Artax01@users.noreply.github.com>
2024-09-30 21:23:25 +0200

    Add files via upload

---
+++ sources/api_request/gen.js
@@ -0,0 +1,24 @@
+// Send request to api.qrserser.com
+
+function genQR(container, data, size) {
+  var text = String(data);
+  var imgSize = String(size);
+
+  var api_url = new Request(`https://api.qrserver.com/v1/create-qr-code/?data=${text}&size=${imgSize}x${imgSize}`);
+
+  fetch(api_url)
+    .then(response => {
+      if (!response.ok) {
+        alert('Something went wrong with API server! Please reload website.');
+        throw new Error('Network response was not ok.');
+      }
+      return response;
+    })
+    .then(data => {
+      container.src = String(data.url);
+      container.title = text;
+    })
+    .catch(error => {
+      console.error('Error', error);
+    });
+};
