7755731e752f23f765d47a766284434cc4ec6b62
Artax <158834512+Artax01@users.noreply.github.com>
2024-10-05 18:20:42 +0200

    update sideMenu.js

--- sources/scripts/sideMenu.js
+++ sources/scripts/sideMenu.js
@@ -6,11 +6,10 @@ document.addEventListener('DOMContentLoaded', function () {
     var blur = document.getElementById('blur');
     
     if (burger_menu && side_menu && blur) {
-        burger_menu.addEventListener('click', function (event) {
+        burger_menu.addEventListener('click', function () {
             burger_menu.classList.toggle('active');
             side_menu.classList.toggle('active');
             blur.classList.toggle('active');
-            event.preventDefault();
         });
         blur.addEventListener('click', function () {
             burger_menu.classList.toggle('active');
@@ -19,10 +18,9 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
     if (darkMode) {
-        darkMode.addEventListener('click', function (event) {
+        darkMode.addEventListener('click', function () {
             document.body.classList.toggle('dark_theme');
             darkMode.classList.toggle('active');
-            event.preventDefault();
         });
     }
 });
